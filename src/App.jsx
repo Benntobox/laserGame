a1347619d16ddffa93c169ccb192c6677904efaa
Benntobox <benjaminsboyle3@gmail.com>
2021-06-08 23:52:28 -0700

    Added game grid connection to app display

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import Game from './laserGame.js'
+import Game from './Game.js'
 
 class App extends React.Component {
   constructor() {
     super();
-    this.game = new Game();
+    this.game = new Game(8);
+  }
+
+  add(piece, position, direction) {
+    this.game.add(piece, position, direction);
   }
 
   render() {
-    return (<div>DEFAULT BEHAVIOUR</div>)
+    let display = this.game.display();
+    return (<div>
+      {display.map((val,index) => <div className='square' key={index+'s'}>{val}</div>)}
+    </div>)
   }
 }
 
--- src/Game.js
+++ src/Game.js
@@ -3,15 +3,14 @@ function Laser(dir, color) { return { dir, color }; }
 const DIRECTION = { 'UP': [-1, 0], 'DOWN': [1, 0], 'LEFT': [0, -1], 'RIGHT': [0, 1] }
 function findNewPos(pos, dir) { return [pos[0] + dir[0], pos[1] + dir[1]] }
 
-class Game extends React.Component {
+class Game {
   constructor(side) {
-    super(side)
     this.side = side;
     this.grid = []
     for (let x = 0; x < side; x++) {
       let row = []
       for (let y = 0; y < side; y++) {
-        row[y] = new Square([x, y], DIRECTION.DOWN);
+        row[y] = x+y*10;
       }
       this.grid[x] = row;
     }
@@ -23,12 +22,8 @@ class Game extends React.Component {
     this.grid[x][y].receiveLaser(laser);
   }
 
-  render() {
-    return (
-    this.grid.map(row => {
-      return <div>{row.map(sq => <div>{sq.pos}</div>)}</div>
-    })
-    )
+  display() {
+    return this.grid.flat();
   }
   
 }
